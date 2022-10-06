3e53ee1e6867de8b274aec4be4a9794ff0fa6ebf
Nick Hallahan <nick@theoutpost.io>
2022-10-06 14:53:09 -0700

    Getting info about drawn features

--- planet_electron/src/simple.js
+++ planet_electron/src/simple.js
@@ -1,8 +1,17 @@
 const api = require('../../planet_node/index')
 const style = require('../styles/default.json')
 
-window.maplibregl.setPlanetVectorTilePlugin(api)
-window.map = new window.maplibregl.Map({
+const maplibre = window.maplibregl
+
+maplibre.setPlanetVectorTilePlugin(api)
+
+const map = window.map = new window.maplibregl.Map({
   container: 'map',
   style: style,
+})
+
+map.on('mouseup', e => {
+  const features = map.queryRenderedFeatures(e.point)
+  console.log('features', features)
+  document.getElementById('features').innerHTML = JSON.stringify(features, null, 2)
 })
