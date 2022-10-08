bcf967b0993751bf2259da6131fd3fd7704a4834
Nick Hallahan <nick@theoutpost.io>
2022-10-08 11:28:50 -0700

    Now able to load electron

---
+++ electron/simple.js
@@ -0,0 +1,17 @@
+const api = require('../index');
+const style = require('../styles/default.json');
+
+const maplibre = window.maplibregl;
+
+maplibre.setPlanetVectorTilePlugin(api);
+
+const map = (window.map = new window.maplibregl.Map({
+    container: 'map',
+    style: style,
+}));
+
+map.on('mouseup', e => {
+    const features = map.queryRenderedFeatures(e.point);
+    console.log('features', features);
+    document.getElementById('features').innerHTML = JSON.stringify(features, null, 2);
+});
