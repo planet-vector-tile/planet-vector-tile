7dae638bdabe24acdcf8034eca0936ebeee7c303
Nick Hallahan <nick@theoutpost.io>
2022-10-15 09:16:21 -0700

    Improve simple UI.

--- electron/simple.js
+++ electron/simple.js
@@ -13,5 +13,9 @@ const map = (window.map = new window.maplibregl.Map({
 map.on('mouseup', e => {
     const features = map.queryRenderedFeatures(e.point);
     console.log('features', features);
+    const el = document.getElementById('features')
     document.getElementById('features').innerHTML = JSON.stringify(features, null, 2);
+    document.getElementById('features-panel').style.display = 'block'
 });
+
+document.getElementById('close-panel').onclick = () => document.getElementById('features-panel').style.display = 'none'
