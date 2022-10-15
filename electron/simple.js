18abd0a1afb4e42d20369fc5ae0a71b597245ab8
Nick Hallahan <nick@theoutpost.io>
2022-10-15 09:53:43 -0700

    Satellite slider

--- electron/simple.js
+++ electron/simple.js
@@ -11,11 +11,18 @@ const map = (window.map = new window.maplibregl.Map({
 }));
 
 map.on('mouseup', e => {
-    const features = map.queryRenderedFeatures(e.point);
-    console.log('features', features);
-    const el = document.getElementById('features')
-    document.getElementById('features').innerHTML = JSON.stringify(features, null, 2);
+    const features = map.queryRenderedFeatures(e.point)
+    if (features.length === 0) {
+        return
+    }
+    document.getElementById('features').innerHTML = JSON.stringify(features, null, 2)
     document.getElementById('features-panel').style.display = 'block'
 });
 
+const satSlider = document.getElementById('sat-slider')
+satSlider.addEventListener('change', e => {
+    const opacity = parseFloat(satSlider.value)
+    console.log('sat opacity', opacity)
+    map.setPaintProperty('sat', 'raster-opacity', opacity)
+})
 document.getElementById('close-panel').onclick = () => document.getElementById('features-panel').style.display = 'none'
