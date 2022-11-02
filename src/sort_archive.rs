291617231ca8d9a0e9bb3e3eb6ebfa68e7b9fd61
Nick Hallahan <nick@theoutpost.io>
2022-11-01 18:57:31 -0700

    Getting pretty far. Memory issue with sorting nodes...

--- src/sort_archive.rs
+++ src/sort_archive.rs
@@ -1,5 +1,10 @@
-use std::{io::{Error, ErrorKind}, path::PathBuf};
-
+use std::{io::{Error, ErrorKind}, path::PathBuf, time::Instant};
+use geo::{coord, Polygon};
+use fast_hilbert::xy2h;
+use geo::algorithm::interior_point::InteriorPoint;
+use geo::geometry::{Coordinate, LineString};
+use log::info;
+use rayon::prelude::*;
 use crate::mutant::{mutable_slice, Mutant};
 use crate::osmflat::osmflat_generated::osm::{Osm, HilbertWayPair};
 
@@ -14,17 +19,74 @@ pub fn sort(archive: Osm, dir: PathBuf) -> Result<(), Box<dyn std::error::Error>
         },
     };
 
-    let node_pairs = mutable_slice(hilbert_node_pairs);
-
-    let way_pairs = Mutant::<HilbertWayPair>::new(&dir, "hilbert_way_pairs", 20)?;
- 
-    // hexdump -C hilbert_way_pairs
-    for slc in way_pairs.mutable_slice() {
-        slc.set_h(u64::MAX);
-        slc.set_i(0xABC);
-    }
-
     
+    let way_pairs = Mutant::<HilbertWayPair>::new(&dir, "hilbert_way_pairs", 20)?;
+    build_hilbert_way_pairs(way_pairs.mutable_slice(), &archive)?;
+
+    // info!("Sorting hilbert node pairs.");
+    // let t = Instant::now();
+    // let node_pairs = mutable_slice(hilbert_node_pairs);
+    // node_pairs.par_sort_unstable_by_key(|idx| idx.h());
+    // info!("Finished in {} secs.", t.elapsed().as_secs());
+    
+
     Ok(())
 }
 
+fn build_hilbert_way_pairs(way_pairs: &mut [HilbertWayPair], archive: &Osm) -> Result<(), Box<dyn std::error::Error>> {
+    let nodes = archive.nodes();
+    let nodes_index = archive.nodes_index();
+    let ways = archive.ways();
+
+    info!("Building hilbert way pairs.");
+    let t = Instant::now();
+
+    way_pairs
+        .par_iter_mut()
+        .enumerate()
+        .for_each(|(i, pair)| {
+            let way = &ways[i];
+            let refs = way.refs();
+            let len = refs.end - refs.start;
+            let mut coords = Vec::<Coordinate<f64>>::with_capacity(len as usize);
+
+            for r in refs {
+                if let Some(idx) = nodes_index[r as usize].value() {
+                    let node = &nodes[idx as usize];
+                    let lon = node.lon() as f64;
+                    let lat = node.lat() as f64;
+                    coords.push(coord! { x: lon, y: lat });
+                };
+            }
+
+            // Calculate point on surface.
+            // http://libgeos.org/doxygen/classgeos_1_1algorithm_1_1InteriorPointArea.html
+            // https://docs.rs/geo/latest/geo/algorithm/interior_point/trait.InteriorPoint.html
+            // https://github.com/georust/geo/blob/main/geo/src/algorithm/interior_point.rs
+
+            let location = if coords.first() == coords.last() {
+                Polygon::new(LineString::new(coords), vec![]).interior_point()
+            } else {
+                LineString::new(coords).interior_point()
+            };
+
+            if let Some(loc) = location {
+                let x = (loc.x() as i64 + i32::MAX as i64) as u32;
+                let y = (loc.x() as i64 + i32::MAX as i64) as u32;
+                // info!("way point on surface {:#?}", loc);
+                let h = xy2h(x, y, 32);
+
+                pair.set_i(i as u64);
+                pair.set_h(h);
+            } else {
+                eprintln!(
+                "Unable to find point on surface to compute hilbert location for way at index {}.",
+                i
+            );
+            }
+        });
+
+    info!("Finished in {} secs.", t.elapsed().as_secs());
+    Ok(())
+
+}
