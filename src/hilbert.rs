296d37add7ad44b8feb87d9eb19252f6678ffeb0
Nick Hallahan <nick@theoutpost.io>
2022-11-04 10:50:17 -0700

    Put build_leaves into its own function.

--- src/hilbert.rs
+++ src/hilbert.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 
+use crate::mutant::Mutant;
+use crate::osmflat::osmflat_generated::osm::{HilbertNodePair, HilbertWayPair};
 use std::cell::Cell;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
-use crate::mutant::Mutant;
-use crate::osmflat::osmflat_generated::osm::{HilbertNodePair, HilbertWayPair};
 
 // 4^14 = 268,435,456
 // 4^16 = 4,294,967,296
@@ -21,7 +21,6 @@ pub struct HilbertTiles {
 
 impl HilbertTiles {
     pub fn build(dir: &Path, leaf_zoom: u8) -> Result<Self, Box<dyn std::error::Error>> {
-        
         // Leaf zoom must be even
         if leaf_zoom & 1 != 0 {
             eprintln!("leaf zoom not even: {}", leaf_zoom);
@@ -31,87 +30,16 @@ impl HilbertTiles {
             )));
         }
 
-        let tree = Mutant::<NodeTile>::new(dir, "hilbert_tree", 1000)?;
-        let mut m_leaves = Mutant::<LeafTile>::new(dir, "hilbert_leaves", 10_000_000)?;
-        let n_chunks = Mutant::<Chunk>::new(dir, "n_chunks", 1000)?;
-        let w_chunks = Mutant::<Chunk>::new(dir, "w_chunks", 1000)?;
-        let r_chunks = Mutant::<Chunk>::new(dir, "r_chunks", 1000)?;
-        
         let m_node_pairs = Mutant::<HilbertNodePair>::open(dir, "hilbert_node_pairs", true)?;
         let m_way_pairs = Mutant::<HilbertWayPair>::open(dir, "hilbert_way_pairs", true)?;
+        let mut m_leaves = Mutant::<LeafTile>::new(dir, "hilbert_leaves", 10_000_000)?;
 
-        let node_pairs = m_node_pairs.slice();
-        let way_pairs = m_way_pairs.slice();
+        build_leaves(&m_node_pairs, &m_way_pairs, &mut m_leaves, leaf_zoom);
 
-        // Find the lowest hilbert tile
-        
-        let mut lowest_h = node_pairs.first().unwrap().h();
-        let way_h = way_pairs.first().unwrap().h();
-        if way_h < lowest_h {
-            lowest_h = way_h;
-        }
-
-        let mut tile_h = idx_h_to_zoom(lowest_h, leaf_zoom);
-        let mut t_i: usize = 0;
-        let mut n_i: usize = 1;
-        let mut w_i: usize = 1;
-
-        let leaves = m_leaves.mutable_slice();
-        let leaves_len = leaves.len();
-        let node_pairs = m_node_pairs.slice();
-        let node_pairs_len = node_pairs.len();
-        let way_pairs = m_way_pairs.slice();
-        let way_pairs_len = way_pairs.len();
-        while t_i < leaves_len && (n_i < node_pairs_len || w_i < way_pairs_len) {
-
-            leaves[t_i] = LeafTile {
-                first_entity_idx: NWR {
-                    n: n_i as u64,
-                    w: w_i as u32,
-                    r: 0,
-                },
-                first_chunk_idx: NWRChunk {
-                    n: 0,
-                    w: 0,
-                    r: 0,
-                },
-                tile_h,
-            };
-
-            let mut node_tile_h = tile_h;
-            let mut way_tile_h = tile_h;
-    
-            while n_i < node_pairs_len {
-                let p = &node_pairs[n_i];
-                let node_h = p.h();
-                node_tile_h = idx_h_to_zoom(node_h, leaf_zoom);
-                if node_tile_h > tile_h {
-                    break;
-                }
-                n_i += 1;
-            }
-      
-            while w_i < way_pairs_len {
-                let p = &way_pairs[w_i];
-                let way_h = p.h();
-                way_tile_h = idx_h_to_zoom(way_h, leaf_zoom);
-                if way_tile_h > tile_h {
-                    break;
-                }
-                w_i += 1;
-            }
-    
-            if node_tile_h > tile_h && node_tile_h < way_tile_h {
-                tile_h = node_tile_h;
-            } else {
-                tile_h = way_tile_h;
-            }
-
-            t_i += 1;
-        }
-
-        m_leaves.set_len(t_i);
-        m_leaves.trim();
+        let tree = Mutant::<NodeTile>::new(dir, "hilbert_tree", 1000)?;
+        let n_chunks = Mutant::<Chunk>::new(dir, "hilbert_n_chunks", 1000)?;
+        let w_chunks = Mutant::<Chunk>::new(dir, "hilbert_w_chunks", 1000)?;
+        let r_chunks = Mutant::<Chunk>::new(dir, "hilbert_r_chunks", 1000)?;
 
         Ok(Self {
             leaf_zoom,
@@ -122,31 +50,96 @@ impl HilbertTiles {
             r_chunks: Cell::new(r_chunks),
         })
     }
-
-    // pub fn open(dir: &Path, file_name: &str) -> Result<Self> {
-      
-    // }
-
-    // pub fn build(&self) {
-
-    // }
-
 }
 
-fn idx_h_to_zoom(h: u64, z: u8) -> u32 {
-    (h >> 2 * (32 - z)) as u32
+fn build_leaves(
+    m_node_pairs: &Mutant<HilbertNodePair>,
+    m_way_pairs: &Mutant<HilbertWayPair>,
+    m_leaves: &mut Mutant<LeafTile>,
+    leaf_zoom: u8,
+) {
+    let node_pairs = m_node_pairs.slice();
+    let way_pairs = m_way_pairs.slice();
+
+    // Find the lowest hilbert tile
+    let mut lowest_h = node_pairs.first().unwrap().h();
+    let way_h = way_pairs.first().unwrap().h();
+    if way_h < lowest_h {
+        lowest_h = way_h;
+    }
+
+    let mut tile_h = zoom_h(lowest_h, 32, leaf_zoom);
+    let mut t_i: usize = 0;
+    let mut n_i: usize = 1;
+    let mut w_i: usize = 1;
+
+    let leaves = m_leaves.mutable_slice();
+    let leaves_len = leaves.len();
+    let node_pairs = m_node_pairs.slice();
+    let node_pairs_len = node_pairs.len();
+    let way_pairs = m_way_pairs.slice();
+    let way_pairs_len = way_pairs.len();
+    while t_i < leaves_len && (n_i < node_pairs_len || w_i < way_pairs_len) {
+        leaves[t_i] = LeafTile {
+            first_entity_idx: NWR {
+                n: n_i as u64,
+                w: w_i as u32,
+                r: 0,
+            },
+            first_chunk_idx: NWRChunk { n: 0, w: 0, r: 0 },
+            tile_h,
+        };
+
+        let mut node_tile_h = tile_h;
+        let mut way_tile_h = tile_h;
+
+        while n_i < node_pairs_len {
+            let p = &node_pairs[n_i];
+            let node_h = p.h();
+            node_tile_h = zoom_h(node_h, 32, leaf_zoom);
+            if node_tile_h > tile_h {
+                break;
+            }
+            n_i += 1;
+        }
+
+        while w_i < way_pairs_len {
+            let p = &way_pairs[w_i];
+            let way_h = p.h();
+            way_tile_h = zoom_h(way_h, 32, leaf_zoom);
+            if way_tile_h > tile_h {
+                break;
+            }
+            w_i += 1;
+        }
+
+        if node_tile_h > tile_h && node_tile_h < way_tile_h {
+            tile_h = node_tile_h;
+        } else {
+            tile_h = way_tile_h;
+        }
+
+        t_i += 1;
+    }
+
+    m_leaves.set_len(t_i);
+    m_leaves.trim();
+}
+
+fn zoom_h(h: u64, from_z: u8, to_z: u8) -> u32 {
+    (h >> 2 * (from_z - to_z)) as u32
 }
 
 struct NWR {
     n: u64,
     w: u32,
-    r: u32
+    r: u32,
 }
 
 struct LeafTile {
     first_entity_idx: NWR,
     first_chunk_idx: NWRChunk,
-    tile_h: u32, // a the leaf zoom
+    tile_h: u32, // At the leaf zoom
 }
 
 struct NodeTile {
@@ -183,19 +176,15 @@ struct Chunk {
 // https://stackoverflow.com/questions/28127165/how-to-convert-struct-to-u8
 
 unsafe fn to_bytes<T: Sized>(p: &T) -> &[u8] {
-    ::std::slice::from_raw_parts(
-        (p as *const T) as *const u8,
-        ::std::mem::size_of::<T>(),
-    )
+    ::std::slice::from_raw_parts((p as *const T) as *const u8, ::std::mem::size_of::<T>())
 }
 
-
+#[cfg(test)]
 mod tests {
     use std::path::PathBuf;
 
-    #[allow(unused_imports)]
     use super::*;
-    
+
     #[test]
     fn test_struct_binary() {
         let c = Chunk {
@@ -211,19 +200,17 @@ mod tests {
 
         let p = PathBuf::from("/Users/n/tmp");
         let chunks = Mutant::<Chunk>::new(&p, "test", 1000).unwrap();
-        
+
         let s = chunks.mutable_slice();
         let s0 = &mut s[0];
         s0.offset = 0x11111111;
         s0.len = 0x22222222;
 
-
         let slc2 = chunks.slice();
         let s2 = &slc2[0];
 
-        let str2 = format!("{:x?}", unsafe { to_bytes(s2)});
+        let str2 = format!("{:x?}", unsafe { to_bytes(s2) });
         assert_eq!(str2, "[11, 11, 11, 11, 22, 22, 22, 22]");
-        println!("{:x?}", unsafe { to_bytes(s0) } );
     }
 
     #[test]
@@ -243,6 +230,4 @@ mod tests {
         assert!(mask_has(m3, 5));
         assert!(mask_has(m3, 15));
     }
-
 }
-
