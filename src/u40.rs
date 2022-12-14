9f4617cc445b56894e9c8cd7806ba8f8644f12cf
Nick Hallahan <nick@theoutpost.io>
2022-12-13 17:52:41 -0800

    Implement basic idea for U40.

--- src/u40.rs
+++ src/u40.rs
@@ -1,24 +1,24 @@
+#![allow(dead_code)]
 
 #[repr(packed)]
 #[derive(Clone, Copy, Debug)]
 pub struct U40 {
     pub low: u32,
-    pub high: u8
+    pub high: u8,
 }
 
 impl U40 {
     pub fn new(v: u64) -> Self {
         Self {
             low: v as u32,
-            high: (v >> 8) as u8
+            high: (v >> 32) as u8,
         }
     }
 
     pub fn v(&self) -> u64 {
-        (self.high as u64) << 8 & self.low as u64
+        (self.high as u64) << 10 | self.low as u64
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -27,10 +27,16 @@ mod tests {
     #[test]
     fn test_basic_u40() {
         let a: u64 = 0x1FFFFFFFF;
+        // println!("{:x}", a);
         let u40 = U40::new(a);
-        println!("{:?}", u40);
+        // println!("{:?}", u40);
+        let h = u40.high;
+        let l = u40.low;
+        assert_eq!(h, 0x1);
+        assert_eq!(l, 0xFFFFFFFF);
+        // println!("high {:x} low {:x}", h, l);
         let b = u40.v();
-        println!("{}", b);
+        // println!("{}", b);
+        assert_eq!(a, b);
     }
-
 }
