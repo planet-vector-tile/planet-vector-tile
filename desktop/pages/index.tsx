f55bfa6bafa2b5b8b7fec6eb54a00ac7926045b5
Nick Hallahan <nick@theoutpost.io>
2022-12-15 21:50:35 -0800

    Features panel has x

--- desktop/pages/index.tsx
+++ desktop/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Info from '../components/Info'
+import Info from '../components/Features'
 import Nav from '../components/Nav'
 
 const Home: NextPage = () => {
