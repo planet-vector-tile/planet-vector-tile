e524c8b37290b3a8ad09c3cabc4bdf319a91a54a
Nick Hallahan <nick@theoutpost.io>
2022-12-16 11:20:46 -0800

    Electron is building.

---
+++ desktop/src/main.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+    app.quit();
+}
+
+const isDev = process.env.IS_DEV === 'true';
+
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    // Open the DevTools.
+    if (isDev) {
+        mainWindow.loadURL('http://localhost:5173');
+        // mainWindow.webContents.openDevTools();
+    } else {
+        // mainWindow.removeMenu();
+        mainWindow.loadFile(path.join(__dirname, '../dist', 'index.html'));
+    }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
