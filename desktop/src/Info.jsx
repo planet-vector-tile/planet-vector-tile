18c55838e1d4113d386ad29a1be12f9e14c336db
Nick Hallahan <nick@theoutpost.io>
2022-12-17 13:58:20 -0800

    Beginning layers panel.

--- desktop/src/Info.jsx
+++ desktop/src/Info.jsx
@@ -11,6 +11,8 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 
+import Layers from './Layers'
+
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
   { name: 'Team', href: '#', icon: UsersIcon, current: false },
@@ -129,56 +131,10 @@ export default function Example() {
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className='hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col'>
+      <div className='hidden md:fixed md:right-0 md:flex md:w-80 md:flex-col md:mt-2 md: mr-2'>
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className='flex min-h-0 flex-1 flex-col bg-gray-800'>
-          <div className='flex flex-1 flex-col overflow-y-auto pt-5 pb-4'>
-            <div className='flex flex-shrink-0 items-center px-4'>
-              <img
-                className='h-8 w-auto'
-                src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                alt='Your Company'
-              />
-            </div>
-            <nav className='mt-5 flex-1 space-y-1 px-2'>
-              {navigation.map(item => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                  )}
-                >
-                  <item.icon
-                    className={classNames(
-                      item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
-                      'mr-3 flex-shrink-0 h-6 w-6'
-                    )}
-                    aria-hidden='true'
-                  />
-                  {item.name}
-                </a>
-              ))}
-            </nav>
-          </div>
-          <div className='flex flex-shrink-0 bg-gray-700 p-4'>
-            <a href='#' className='group block w-full flex-shrink-0'>
-              <div className='flex items-center'>
-                <div>
-                  <img
-                    className='inline-block h-9 w-9 rounded-full'
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    alt=''
-                  />
-                </div>
-                <div className='ml-3'>
-                  <p className='text-sm font-medium text-white'>Tom Cook</p>
-                  <p className='text-xs font-medium text-gray-300 group-hover:text-gray-200'>View profile</p>
-                </div>
-              </div>
-            </a>
-          </div>
+          <Layers />
         </div>
       </div>
       <div className='flex flex-1 flex-col md:pl-64'>
