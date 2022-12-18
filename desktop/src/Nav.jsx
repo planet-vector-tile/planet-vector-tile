5f9cbc18333e9ec7dd31ce5dc8b80e196a624ff7
Nick Hallahan <nick@theoutpost.io>
2022-12-17 17:08:10 -0800

    Thinner nav

--- desktop/src/Nav.jsx
+++ desktop/src/Nav.jsx
@@ -11,123 +11,60 @@ export default function Nav() {
     <Disclosure as='nav' className='bg-gray-800'>
       {({ open }) => (
         <>
-          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-            <div className='flex h-16 items-center justify-between'>
+          <div className='mx-auto pl-20 pr-6'>
+            <div className='flex h-10 items-center justify-between'>
               <div className='flex items-center'>
                 <div className='flex-shrink-0'>
-                  <img
-                    className='block h-8 w-auto lg:hidden'
-                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                    alt='Your Company'
-                  />
-                  <img
-                    className='hidden h-8 w-auto lg:block'
-                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                    alt='Your Company'
-                  />
+                  <svg className='w-8 h-8' strokeWidth='1.5' viewBox='0 0 24 24' fill='none'>
+                    <circle cx='12' cy='12' r='8' stroke='white' stroke-width='1.5'></circle>
+                    <path
+                      d='M17.5 6.348c2.297-.538 3.945-.476 4.338.312.73 1.466-3.158 4.89-8.687 7.645-5.528 2.757-10.602 3.802-11.333 2.336-.392-.786.544-2.134 2.349-3.64'
+                      stroke='white'
+                    ></path>
+                  </svg>
                 </div>
                 <div className='hidden sm:ml-6 sm:block'>
                   <div className='flex space-x-4'>
-                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                    <a href='#' className='rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white'>
+                    <a href='#' className='rounded-md bg-gray-900 px-3 py-1 text-sm font-medium text-white'>
                       Dashboard
                     </a>
                     <a
                       href='#'
-                      className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                      className='rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
                     >
-                      Team
+                      First part
                     </a>
                     <a
                       href='#'
-                      className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                      className='rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
                     >
                       Projects
                     </a>
                     <a
                       href='#'
-                      className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                      className='rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
                     >
                       Calendar
                     </a>
                   </div>
                 </div>
               </div>
-              <div className='hidden sm:ml-6 sm:block'>
-                <div className='flex items-center'>
-                  <button
-                    type='button'
-                    className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                  >
-                    <span className='sr-only'>View notifications</span>
-                    <BellIcon className='h-6 w-6' aria-hidden='true' />
-                  </button>
 
-                  {/* Profile dropdown */}
-                  <Menu as='div' className='relative ml-3'>
-                    <div>
-                      <Menu.Button className='flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                        <span className='sr-only'>Open user menu</span>
-                        <img
-                          className='h-8 w-8 rounded-full'
-                          src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                          alt=''
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter='transition ease-out duration-100'
-                      enterFrom='transform opacity-0 scale-95'
-                      enterTo='transform opacity-100 scale-100'
-                      leave='transition ease-in duration-75'
-                      leaveFrom='transform opacity-100 scale-100'
-                      leaveTo='transform opacity-0 scale-95'
+              <div className='hidden sm:ml-6 sm:block'>
+                  <span className='isolate inline-flex rounded-md shadow-sm'>
+                    <button
+                      type='button'
+                      className='relative inline-flex items-center border border-gray-700 rounded-l-md px-2 py-1 text-sm font-medium bg-gray-900 text-white'
                     >
-                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href='#'
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Your Profile
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href='#'
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href='#'
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Sign out
-                            </a>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
+                      Layers
+                    </button>
+                    <button
+                      type='button'
+                      className='relative inline-flex items-center border border-gray-700 rounded-r-md px-2 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                    >
+                      Features
+                    </button>
+                  </span>
               </div>
               <div className='-mr-2 flex sm:hidden'>
                 {/* Mobile menu button */}
@@ -158,7 +95,7 @@ export default function Nav() {
                 href='#'
                 className='block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
               >
-                Team
+                Disclosure Button
               </Disclosure.Button>
               <Disclosure.Button
                 as='a'
