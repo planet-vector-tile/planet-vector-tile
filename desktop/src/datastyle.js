60d39445d2341f1dfce821aefbc38e1a5307da7d
Nick Hallahan <nick@theoutpost.io>
2022-12-20 19:43:01 -0800

    Rendering data layers from both planet and mvt layers.

--- desktop/src/datastyle.js
+++ desktop/src/datastyle.js
@@ -7,59 +7,73 @@ window.datastyle = sources
 
 export default function initFromMap(map) {
   map.on('sourcedata', function (e) {
-    let styleNeedsUpdate = false
-    const tileData = e?.tile?.latestFeatureIndex?.vtLayers
-    if (!tileData) {
+    const newLayers = []
+    const layerIds = e?.tile?.latestFeatureIndex?.layerIds
+    if (!Array.isArray(layerIds) || layerIds.length === 0) {
       return
     }
-    console.log('sourcedata', e.sourceId)
-    const layerKeys = Object.keys(tileData)
-    let source = sources[e.sourceId]
-    if (!source) {
-      source = {}
-      sources[e.sourceId] = source
+
+    let sourceLayerIds = sources[e.sourceId]
+    if (!sourceLayerIds) {
+      sourceLayerIds = findExistingLayerIdsFromStyle(e.sourceId, store.dataStyle)
+      sources[e.sourceId] = sourceLayerIds
     }
 
-    for (const layerId of layerKeys) {
-      const layer = source[layerId]
-      // this is a new layer for us to track
-      if (!layer) {
-        source[layerId] = determineGeometryType(tileData[layerId])
-        styleNeedsUpdate = true
-      }
-      // we know about the layer, but we don't know what type it is
-      if (layer === 'Unknown') {
-        const geomType = determineGeometryType(tileData[layerId])
-        if (geomType !== 'Unknown') {
-          source[layerId] = geomType
-          styleNeedsUpdate = true
-        }
+    for (const name of layerIds) {
+      if (!sourceLayerIds.has(name)) {
+        sourceLayerIds.add(name)
+        newLayers.push(name)
       }
     }
 
-    if (styleNeedsUpdate) {
-      updateStyle()
-    }
+    updateStyle(map, e.sourceId, newLayers)
   })
 }
 
-function determineGeometryType(layer) {
-  if (layer.features.length > 0) {
-    const feature = layer.features[0]
-    if (feature.type === 1) {
-      return 'Point'
-    }
-    if (feature.type === 2) {
-      return 'LineString'
-    }
-    if (feature.type === 3) {
-      return 'Polygon'
+function findExistingLayerIdsFromStyle(sourceId, style) {
+  const layerIds = new Set()
+  for (const layer of style.layers) {
+    if (layer.source === sourceId) {
+      layerIds.add(layer.id)
     }
   }
-  return 'Unknown'
+  return layerIds
 }
 
-function updateStyle() {
-  console.log('updateStyle')
-  console.log('sources', sources)
+function updateStyle(map, sourceId, newLayers) {
+  const style = store.dataStyle
+
+  for (const layerId of newLayers) {
+    const lineLayer = {
+      id: `${layerId}_line`,
+      type: 'line',
+      minzoom: 0,
+      maxzoom: 23,
+      source: sourceId,
+      'source-layer': layerId,
+      paint: {
+        'line-color': 'pink',
+        'line-width': 2,
+      },
+    }
+
+    const circleLayer = {
+      id: `${layerId}_circle`,
+      type: 'circle',
+      minzoom: 0,
+      maxzoom: 23,
+      source: sourceId,
+      'source-layer': layerId,
+      paint: {
+        'circle-radius': 4,
+        'circle-color': 'pink',
+      },
+    }
+
+    style.layers.push(lineLayer)
+    style.layers.push(circleLayer)
+
+    map.addLayer(lineLayer)
+    map.addLayer(circleLayer)
+  }
 }
