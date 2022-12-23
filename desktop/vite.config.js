7a20cda19519bd58143573957d56504db8a39501
Nick Hallahan <nick@theoutpost.io>
2022-12-23 09:23:52 -0800

    Set up typescript

--- desktop/vite.config.js
+++ desktop/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
     base: process.env.IS_DEV !== 'true' ? './' : '/',
     plugins: [react()],
