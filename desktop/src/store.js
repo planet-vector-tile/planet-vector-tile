6fa1a3a914719d1bdf824fa653d00ae5cf521852
Nick Hallahan <nick@theoutpost.io>
2022-12-24 10:49:45 -0800

    Explain proxy in store.

--- desktop/src/store.js
+++ desktop/src/store.js
@@ -1,3 +1,21 @@
+// The store is the contents of localStorage inflated into JavaScript objects.
+// We are using a Proxy to intercept writes to the store and persist them to localStorage.
+// The removes the need of constantly having to explicitly serde JSON for the local storage key values.
+// Note that the Proxy only applies to the top level items of the store object, so you do need to
+// explicitly assign a value to the store object for the persistenct to occur. e.g.:
+//
+//    store.nav = nav
+//
+// Mutating a sub object will not cause a write.
+//
+//    store.nav.something = 'foo' // no write to localStore
+//
+// But then doing this will cause a write:
+//
+//    store.nav = store.nav
+//
+// Keeping this explicit with one level of Proxy is important, because JSON serde does have a cost.
+
 // When the app is packaged, the pwd is in the dist dir
 let mapStylePath = '../styles/default.json'
 let dataStylePath = '../styles/data.json'
