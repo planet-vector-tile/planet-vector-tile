8268ae96cfd3f0b2c7356eaba0c884dd49ce95c7
Nick Hallahan <nick@theoutpost.io>
2023-01-13 16:42:29 -0800

    Shortcut past building hilbert way pairs.

--- src/sort.rs
+++ src/sort.rs
@@ -34,11 +34,12 @@ pub fn sort_flatdata(flatdata: Osm, dir: &PathBuf) -> Result<(), Box<dyn std::er
     };
 
     // Build hilbert way pairs.
-    let ways = flatdata.ways();
-    let ways_len = flatdata.ways().len();
-    let m_way_pairs = Mutant::<HilbertWayPair>::new(dir, "hilbert_way_pairs", ways_len)?;
-    let way_pairs = m_way_pairs.mutable_slice();
-    build_hilbert_way_pairs(way_pairs, &flatdata)?;
+    // let ways = flatdata.ways();
+    // let ways_len = flatdata.ways().len();
+    // let m_way_pairs = Mutant::<HilbertWayPair>::new(dir, "hilbert_way_pairs", ways_len)?;
+    // let way_pairs = m_way_pairs.mutable_slice();
+    // build_hilbert_way_pairs(way_pairs, &flatdata)?;
+    let m_way_pairs = Mutant::<HilbertWayPair>::open(dir, "hilbert_way_pairs", true).unwrap();
 
     // Build hilbert relation pairs
     let relations_len = flatdata.relations().len();
